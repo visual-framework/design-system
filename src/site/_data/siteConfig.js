730f57e61054c8c5c30e2e34fceb5dee0a983398
Ken Hawkins <khawkins98@gmail.com>
2019-06-25 11:51:08 +0200

    Typo in email

--- src/site/_data/siteConfig.js
+++ src/site/_data/siteConfig.js
@@ -5,6 +5,6 @@ module.exports = {
     short_description: 'The laboratory’s design system for communications in all media',
     url: "https://heuristic-swirles-12e12d.netlify.com/",
     author: "EMBL Digital communications",
-    email: "digital-commss@embl.org",
+    email: "digital-comms@embl.org",
   }
 };
