092bde774631a345b1039c5690224f8bf59b9dbf
Ken Hawkins <khawkins98@gmail.com>
2019-08-28 19:48:35 +0200

    Update deployment path

--- eleventy.js
+++ eleventy.js
@@ -87,7 +87,7 @@ module.exports = function(config) {
     htmlTemplateEngine : ["njk", "md"],
     markdownTemplateEngine : "njk",
     passthroughFileCopy: true,
-    pathPrefix: "/" // if your site is deployed to a sub-url, otherwise comment out
+    pathPrefix: "/guidelines/design/" // if your site is deployed to a sub-url, otherwise comment out
   };
 
 };
