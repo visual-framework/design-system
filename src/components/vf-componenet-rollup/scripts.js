fc9ec1cf43e1490b9924e2295d073261893f3267
Ken Hawkins <khawkins98@gmail.com>
2020-09-03 17:51:36 +0200

    Update and add components

--- src/components/vf-componenet-rollup/scripts.js
+++ src/components/vf-componenet-rollup/scripts.js
@@ -28,6 +28,12 @@ vfTree();
 import { vfSearchClientSide } from 'vf-search-client-side/vf-search-client-side';
 // No default invokation
 
+import { vfShowMore } from 'vf-show-more/vf-show-more';
+vfShowMore();
+
+import { vfLocationNearest } from 'vf-location-nearest/vf-location-nearest';
+// Not invoked by default
+
 // All EMBL JS
 import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-content-hub-loader__html-imports';
 import { emblContentHubFetch } from 'embl-content-hub-loader/embl-content-hub-loader__fetch';
